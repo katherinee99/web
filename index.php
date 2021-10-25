42bfb7ee530fb97e3c2606a60ab650ad93161a99
katy <pamela121316@gmail.com>
2021-10-25 11:42:08 +0200

    introducido

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
         <meta charset="UTF-8">
         <title>Titulo</title>
         <link rel="stylesheet" href="css/estilo.css">
+
+        <script src="js/prog.js"></script>
+        
     </head>
     <body>
+        <pre>
+            Ejecuta php -S localhost:9000
+            visitalo con tu navegador
+        </pre>
+
         <h1>esto es un Titulo</h1>
         <p>esto es un parrafo</p>
+        <?php 
+        echo "Esto lo escribe <b>PHP</b>" 
+        ?>
     </body>
 </html>
